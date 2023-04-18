fac8314eed1fcb298e6761324e8f8ff408371191
husamsaad123 <125978708+husamsaad123@users.noreply.github.com>
2023-04-18 19:10:22 +0300

    Create rainbow_words.py

---
+++ rainbow_words.py
@@ -0,0 +1,5 @@
+# Create a list of rainbow words
+words = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+# Print a sentence using the rainbow words
+print("I love the colors of the rainbow. My favorite color is " + words[3] + ".")
